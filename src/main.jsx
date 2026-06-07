03bee85c5a2047db214754a7495e0d724cf8c959
Khizar Ahmad <devworkspace3300@gmail.com>
2026-06-07 19:58:55 +0500

    feat: configure Capacitor and convert project to Progressive Web App (PWA)

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Roomzy',
+        short_name: 'Roomzy',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#173663',
+        icons: [
+          {
+            src: '/icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
